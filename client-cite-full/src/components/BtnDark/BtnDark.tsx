881e78a86134870739cf89136a15529e198bd988
Nigar Hasanova <nigarhasanova236@gmail.com>
2026-04-17 14:13:39 +0400

    done

--- client-cite-full/src/components/BtnDark/BtnDark.tsx
+++ client-cite-full/src/components/BtnDark/BtnDark.tsx
@@ -3,14 +3,15 @@ import classes from './btnDark.module.css'
 type Props = {
     title: string,
     disabled?: boolean | undefined,
-    type : string
+    type?: 'button' | 'submit' | 'reset'
 }
 
-export default function BtnDark({ title, disabled , type}: Props) {
+export default function BtnDark({ title, disabled, type = 'button' }: Props) {
     return (
         <button
             className={classes.btnDark}
             disabled={disabled}
+            type={type}
         >
             {title}
         </button>
